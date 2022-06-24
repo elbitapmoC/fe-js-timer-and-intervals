77a66b67f7ab87d00976f62e5652200337988dd7
Ahkal <bp@sojourn.llc>
2022-06-24 14:32:53 -0400

    Foundation set

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# fe-js-promises
 # fe-js-timer-and-intervals
---
+++ script.js
@@ -0,0 +1,28 @@
+const start = document.getElementById('start');
+const stop = document.getElementById('stop');
+const count = document.getElementById('count');
+
+start.addEventListener('click', startTimer);
+stop.addEventListener('click', stopTimer);
+
+// let timerID;
+let animationFrameID;
+
+setTimeout(() => {
+  console.log(performance.now());
+  console.log(date.now()); //# of ms since Jan 1, 1970
+}, 1000);
+
+// setInterval & setTimeout - BOTH functions of the window.
+function startTimer(){
+  // timerID = setInterval(()=>{
+  //   count.textContent++;
+  // }, 300)
+  count.textContent++;
+  animationFrameID = requestAnimationFrame(startTimer);
+}
+
+function stopTimer(){
+  // clearInterval(animationFrameID);
+  cancelAnimationFrame(animationFrameID);
+}
